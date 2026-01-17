97fa1dab78ae72b0b6fd33eb1bda1d5b166d1049
Kroos Huang <hdinever@gmail.com>
2026-01-17 09:22:04 +0800

    branch推送

--- constants.ts
+++ constants.ts
@@ -2,14 +2,14 @@ import { BossData } from './types';
 
 // 明日方舟Boss头像 - SKZ奖池专用
 export const BOSSES: BossData[] = [
-  { id: '3-8', name: '大号工程版活动蚀刻章', img: '/skz-pool/3-8.jpg', color: '#94a3b8' },
-  { id: '6-16', name: '限定三号透明蚀刻章', img: '/skz-pool/6-16.jpg', color: '#bae6fd' },
-  { id: '7-18', name: '限定8号透明蚀刻章', img: '/skz-pool/7-18.jpg', color: '#ef4444' },
-  { id: 'WD-8', name: '额外两个吧唧自选', img: '/skz-pool/WD-8.jpg', color: '#1e293b' },
-  { id: 'RS-8', name: '再自选2个普通蚀刻章', img: '/skz-pool/RS-8.jpg', color: '#a78bfa' },
-  { id: 'BI-8', name: '龙门蚀刻章+如我所见蚀刻章', img: '/skz-pool/BI-8.jpg', color: '#3b82f6' },
-  { id: 'AS-9', name: '一个普通蚀刻章自选+再抽一次', img: '/skz-pool/AS-9.jpg', color: '#fde047' },
-  { id: 'MB-8', name: '紫外线手电筒+普通蚀刻章或吧唧', img: '/skz-pool/MB-8.jpg', color: '#fb923c' }
+  { id: '1', name: '大号工程版活动蚀刻章', img: '/skz-pool/大号工程版活动蚀刻章.jpg', color: '#94a3b8' },
+  { id: '2', name: '限定三号透明蚀刻章', img: '/skz-pool/限定三号透明蚀刻章.jpg', color: '#bae6fd' },
+  { id: '3', name: '限定8号透明蚀刻章', img: '/skz-pool/限定8号透明蚀刻章.jpg', color: '#ef4444' },
+  { id: '4', name: '额外两个吧唧自选', img: '/skz-pool/额外两个吧唧自选.jpg', color: '#1e293b' },
+  { id: '5', name: '再自选2个普通蚀刻章', img: '/skz-pool/再自选2个普通蚀刻章.jpg', color: '#a78bfa' },
+  { id: '6', name: '龙门蚀刻章+如我所见蚀刻章', img: '/skz-pool/龙门蚀刻章+如我所见蚀刻章.jpg', color: '#3b82f6' },
+  { id: '7', name: '一个普通蚀刻章自选+再抽一次', img: '/skz-pool/一个普通蚀刻章自选+再抽一次.jpg', color: '#fde047' },
+  { id: '8', name: '紫外线手电筒+普通蚀刻章或吧唧', img: '/skz-pool/紫外线手电筒+普通蚀刻章或吧唧.jpg', color: '#fb923c' }
 ];
 
 // Arknights Theme Colors (Light Theme)
